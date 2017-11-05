d3dabe6e5ad94f141b4117168ea061ec0ace1294
Aurvo <dan@sdcsol.com>
2017-11-04 21:22:15 -0400

    final writetolog removal

--- app.py
+++ app.py
@@ -186,7 +186,6 @@ def gconnect():
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
-    writeToLog(url)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # If there was an error in the access token info, abort.
